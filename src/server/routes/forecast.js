f8dacafac8af447966620c8ec399a3763f9ce1ad
j- <j@skeoh.com>
2016-07-07 21:40:43 +1000

    Add forecast.io service

--- src/server/routes/forecast.js
+++ src/server/routes/forecast.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
-const request = require('request');
-
-const APIKEY = process.env.APIKEY;
+const Forecast = require('forecast-io');
+const forecast = new Forecast(process.env.APIKEY);
 
 app.get('/forecast/:nonce/:lat,:lon', (req, res) => {
-	if (!APIKEY) {
-		throw new Error('API key was not found');
-	}
-
 	const lat = req.params.lat;
 	const lon = req.params.lon;
 	if (!lat || !lon) {
 		throw new Error('Required fields `lat` and `lon` not found');
 	}
 
-	request.get(
-		`https://api.forecast.io/forecast/${APIKEY}/${lat},${lon}`
-	).pipe(res);
+	forecast
+		.latitude(lat)
+		.longitude(lon)
+		.units('si')
+		.get()
+		.then((data) => res.send(data))
+		.catch((err) => {
+			throw err;
+		});
 });
 
 module.exports = app;
