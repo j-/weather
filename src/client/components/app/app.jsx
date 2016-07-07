04b6f7abc84964062086304fa50b743b267ab03b
j- <j@skeoh.com>
2016-07-07 19:03:44 +1000

    Add current location component to application

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CurrentLocation from '../current-location';
 
 const App = () => (
 	<div className="app">
 		<h1>Weather Forecast</h1>
+		<CurrentLocation />
 	</div>
 );
 
